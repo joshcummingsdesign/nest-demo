7089c89175a78e4d082c2fd2301431a700807e58
joshcummingsdesign <joshcummingsdesign@gmail.com>
2020-06-16 18:46:07 -0700

    Add security middleware

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as helmet from 'helmet';
+import * as rateLimit from 'express-rate-limit';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(helmet());
+
+  app.use(
+    rateLimit({
+      windowMs: 15 * 60 * 1000,
+      max: 100,
+    }),
+  );
+
   await app.listen(process.env.PORT);
 }
 bootstrap();
