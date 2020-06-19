4f7eed6d60e7bbc6b37a40a1d32ea6e7ac9b6884
joshcummingsdesign <joshcummingsdesign@gmail.com>
2020-06-18 18:46:52 -0700

    Add email module to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,19 +8,20 @@ import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { validationSchema } from './config';
 import { CryptoModule } from './crypto/crypto.module';
+import { EmailModule } from './email/email.module';
 import { AuthModule } from './auth/auth.module';
 import { RoleModule } from './role/role.module';
 import { InstrumentModule } from './instrument/instrument.module';
 import { UserModule } from './user/user.module';
 import { LessonModule } from './lesson/lesson.module';
 import { AvailabilityModule } from './availability/availability.module';
-import { EmailModule } from './email/email.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true, validationSchema }),
     TypeOrmModule.forRoot(),
     CryptoModule,
+    EmailModule,
     AuthModule,
     RoleModule,
     InstrumentModule,
